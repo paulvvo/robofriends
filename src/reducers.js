542914a85f1edea8b6ccd50779742147c4a3d8ad
paulvvo <paulvo@live.ca>
2018-10-29 19:58:39 -0400

    initial commit of robo friends application, basic features include search forname, scrolling component and json placeholder fetches. started to add redux to this application, actions and reducers have been added

---
+++ src/reducers.js
@@ -0,0 +1,17 @@
+import CHANGE_SEARCH_FIELD from "./constants";
+
+
+//store
+const initialState = {
+	searchField:""
+}
+
+//reducers
+const searchRobots = (state=initialState, action={}) =>{
+	switch(action.type){
+		case:CHANGE_SEARCH_FIELD:
+			return Object.assign({}, state, {searchField:action.payload});
+		default:
+			return state;
+	}
+}
