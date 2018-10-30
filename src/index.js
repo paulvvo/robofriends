3f46fa05eedd462810246fbc8699740d2cf83eac
paulvvo <paulvo@live.ca>
2018-10-29 20:27:14 -0400

    added store to index and wrapped the app component with provider

--- src/index.js
+++ src/index.js
@@ -5,6 +5,19 @@ import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
 import {Provider, connect} from "react-redux";
+import {createStore} from "redux";
+import {searchRobots} from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const store = createStore(rootReducer);
+//in an app there wil be many reducers, and you're going to want
+//to have a root reducer for the store, but for now we're going
+// to use just the searchRobots reducer
+const store = createStore(searchRobots);
+//wrapping the app in provider will pass the store down to every
+// other component child underneath app
+ReactDOM.render(
+	<Provider store={store}>
+		<App/>
+	</Provider> , document.getElementById('root'));
+	
 registerServiceWorker();
--- src/reducers.js
+++ src/reducers.js
@@ -1,15 +1,13 @@
-import CHANGE_SEARCH_FIELD from "./constants";
+import {CHANGE_SEARCH_FIELD} from "./constants";
 
 
-//store
 const initialState = {
 	searchField:""
 }
 
-//reducers
-const searchRobots = (state=initialState, action={}) =>{
+export const searchRobots = (state=initialState, action={}) =>{
 	switch(action.type){
-		case:CHANGE_SEARCH_FIELD:
+		case CHANGE_SEARCH_FIELD:
 			return Object.assign({}, state, {searchField:action.payload});
 		default:
 			return state;
