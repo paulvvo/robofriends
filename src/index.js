1b893f07e3d374b487fc7656260b79d2219ed360
paulvvo <paulvo@live.ca>
2018-12-16 20:29:33 -0500

    added redux logger middleware to log actions and console.logs the actions

--- src/index.js
+++ src/index.js
@@ -4,16 +4,22 @@ import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
+import {applyMiddleware} from "redux";
+import {createLogger} from "redux-logger";
 
 import {Provider} from "react-redux";
 import {createStore} from "redux";
 import {searchRobots} from "./reducers";
 
+
+
+const logger = createLogger();
+
 // const store = createStore(rootReducer);
 //in an app there wil be many reducers, and you're going to want
 //to have a root reducer for the store, but for now we're going
 // to use just the searchRobots reducer
-const store = createStore(searchRobots);
+const store = createStore(searchRobots, applyMiddleware(logger));
 //wrapping the app in provider will pass the store down to every
 // other component child underneath app
 ReactDOM.render(
